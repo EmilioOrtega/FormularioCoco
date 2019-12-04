14e10e9976ae480f4e13faa9afb31e8a39eb9701
EmilioOrtega <pancrasio123456@gmail.com>
2019-12-04 00:24:05 -0600

    Solfalta agregar usuario a la base

---
+++ googleForms/src/java/Modelo/ResultadoBean.java
@@ -0,0 +1,31 @@
+package Modelo;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Emilio
+ */
+@ManagedBean
+@RequestScoped
+public class ResultadoBean {
+    private String nombre;
+    private String contra;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+}
