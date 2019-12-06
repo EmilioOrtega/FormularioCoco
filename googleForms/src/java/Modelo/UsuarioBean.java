238b3ee40cd49c34accff65b977708f52055c506
EmilioOrtega <pancrasio123456@gmail.com>
2019-12-06 11:31:24 -0600

    aa

--- googleForms/src/java/Modelo/UsuarioBean.java
+++ googleForms/src/java/Modelo/UsuarioBean.java
@@ -5,9 +5,20 @@
  */
 package Modelo;
 
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.context.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.Table;
 
 /**
  *
@@ -15,16 +26,25 @@ import javax.faces.context.*;
  */
 @ManagedBean
 @RequestScoped
+@Entity
+@Table(name = "usuario")
 public class UsuarioBean {
+
+    @Id
+    @Column(name = "id")
     private int id;
+    @Column(name = "nombre")
     private String nombre;
+    @Column(name = "contra")
     private String contra;
-    
-    public UsuarioBean(){
-        
+
+    List<UsuarioBean> listUser;
+
+    public UsuarioBean() {
+
     }
-    public UsuarioBean(int id, String nombre, String contra) {
-        this.id = id;
+
+    public UsuarioBean(String nombre, String contra) {
         this.nombre = nombre;
         this.contra = contra;
     }
@@ -52,12 +72,24 @@ public class UsuarioBean {
     public void setContra(String contra) {
         this.contra = contra;
     }
-     
-    public void setUsuario(){
+
+    public void setUsuario() {
         FacesContext context = FacesContext.getCurrentInstance();
         ResultadoBean rb = context.getApplication().evaluateExpressionGet(context, "#{resultadoBean}", ResultadoBean.class);
         rb.setNombre(this.nombre);
-        //rb.setContra(this.contra);
+        rb.setContra(this.contra);
+        insertUser();
+    }  
+
+    public void insertUser() {
+        UsuarioBean usuario = new UsuarioBean(this.nombre, this.contra);
+        EntityManagerFactory emf;
+        EntityManager em;
+        emf = Persistence.createEntityManagerFactory("googleFormsPU");
+        em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(usuario);
+        em.getTransaction().commit();
     }
-     
+
 }
